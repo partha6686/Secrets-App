313a9b01a733c331c7bdd6414f642aed9226a39f
Partha <parthasarathi6686@gmail.com>
2021-10-03 13:30:16 +0530

    Level-4 Salting and hashing using bcrypt

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+//const md5 = require("md5");
 //const encrypt = require("mongoose-encryption");
 
+const saltRounds = 10;
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
@@ -37,30 +39,43 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const userDoc = new User({
-        username: req.body.username,
-        password: md5(req.body.password)
+    
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const userDoc = new User({
+            username: req.body.username,
+            password: hash
+        });
+    
+        userDoc.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
-
-    userDoc.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+    
 });
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
+    
 
     User.findOne({username: username},function(err,result){
         if(err){
             console.log(err);
         }else{
-            if(result && password === result.password){
-                res.render("secrets");
+            if(result){
+                bcrypt.compare(password, result.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }else{
+                        res.render("login");
+                    }
+                });
+               
             } else {
                 res.render("login");
             }
